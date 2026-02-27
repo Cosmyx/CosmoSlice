deb89388c543f62ca2fb3fc2f142917cd6fe3bd0
SteepyTheFrenchMaker <steepy.maker@orange.fr>
2026-02-27 17:33:34 +0100

    Forced StealthMode

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -314,11 +314,11 @@ void AppConfig::set_defaults()
     }
 
     // Orca
-    if (get("stealth_mode").empty()) {
-        set_bool("stealth_mode", false);
-    }
-    if (get("allow_abnormal_storage").empty()) {
-        set_bool("allow_abnormal_storage", false);
+    //if (get("stealth_mode").empty()) {
+        set_bool("stealth_mode", true);
+    //}
+    if (get("legacy_networking").empty()) {
+        set_bool("legacy_networking", true);
     }
 #ifdef __linux__
     if (get(SETTING_USE_ENCRYPTED_TOKEN_FILE).empty())
